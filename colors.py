e3c576d16ecc836df78b308f10b88153dbdddfaf
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2022-06-24 06:02:53 +0530

    Initial qtile config

---
+++ colors.py
@@ -0,0 +1,41 @@
+nord_fox = {
+    "bg": "#2e3440",
+    "fg": "#b9bfca",
+    "fg_gutter": "#4b5668",
+    "black": "#3b4252",
+    "red": "#bf616a",
+    "green": "#a3be8c",
+    "yellow": "#ebcb8b",
+    "blue": "#81a1c1",
+    "magenta": "#b48ead",
+    "cyan": "#88c0d0",
+    "white": "#e5e9f0",
+    "orange": "#c9826b",
+    "pink": "#bf88bc",
+}
+onedark = {
+
+    "fg": "#e6e6e6",
+    "bg": "#282c34",
+
+    "red": "#ff5555",
+    "redd": "#ec0101",
+    #: red
+    "green": " #5ebdab",
+    "greend": " #47d4b9",
+    #: green
+    "yellow": " #fea44c",
+    "yellowd": "#ff8a18",
+    #: yellow
+    "blue": " #367bf0",
+    "blued": " #277fff",
+    #: blue
+    "magenta": " #bf2e5d",
+    "magentad": "#d71655",
+    #: magenta
+    "cyan": " #4eeeff",
+    "cyand": "#05a1f7",
+    #: cyan
+    "white": "#ffffff",
+    "whited": " #e6e6e6",
+}
